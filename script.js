022523dbbf099091b893c9bac53ef22dfa7b549d
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-25 16:08:50 +0100

    Modif

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let age = prompt('Quel est votre âge?');
 let reponse = document.getElementById("reponse");
 
-if (age <17) {
+if (age < 17) {
     reponse.innerHTML = "Vous êtes mineur";
 }
 else {
